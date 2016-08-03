601c5eec70c30cd781b1549dbf3983044e79a762
phretaddin <phretaddin@gmail.com>
2016-08-03 12:54:04 -0400

    Added test fixture

---
+++ fixtures/nested-repeat.js
@@ -0,0 +1,11 @@
+module.exports.schema = {
+  "the": [ { "nuts": "string", "qwer": "varuint", "bork": "int16" } ]
+};
+
+module.exports.items = [{
+  "the": [ 
+    { "nuts": "xcvxcvxcvxcvxc", "qwer": 15, "bork": 55 },
+    { "nuts": "cvxcvxcv", "qwer": 23, "bork": 355 },
+    { "nuts": "asdf", "qwer": 423, "bork": 523 }
+  ]
+}];
