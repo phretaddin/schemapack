dc6b2abbf3466473ffcc23762ae6594c2060370b
phretaddin <phretaddin@gmail.com>
2016-08-03 12:54:22 -0400

    Added test fixture

---
+++ fixtures/person-array.js
@@ -0,0 +1,7 @@
+module.exports.schema = [ { "name": "string", "numbers": [ "varint" ], "age": "uint8" } ];
+
+module.exports.items = [[
+  { "name": "joe", "numbers": [ -3, 2, 5 ], "age": 42 },
+  { "name": "john smith iv", "numbers": [], "age": 27 },
+  { "name": "bobby", "numbers": [ -22, 1 ], "age": 6 },
+]];
